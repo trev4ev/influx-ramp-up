bddf34e4d3d7c25cff061db0a5da3c60dbef749b
surbhiijain <30298846+surbhiijain@users.noreply.github.com>
2020-09-20 15:33:00 -0700

    completed functionality

--- main/main.go
+++ main/main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 )
 
@@ -19,7 +20,13 @@ type Member struct {
 	Team string
 }
 
+type Response struct {
+	Status  string
+	Message string
+}
+
 func mainHandler(w http.ResponseWriter, req *http.Request) {
+	// read in request body
 	body, _ := ioutil.ReadAll(req.Body)
 	var params map[string]json.RawMessage
 	err := json.Unmarshal(body, &params)
@@ -27,60 +34,149 @@ func mainHandler(w http.ResponseWriter, req *http.Request) {
 		log.Println("Unable to parse body ", err)
 		return
 	}
-	var member Member
-	err = json.Unmarshal(params["object"], &member)
-	if err != nil {
-		log.Println("Unable to parse object ", err)
-		return
-	}
 
+	// convert function and groupName to strings
 	function, _ := strconv.Unquote(string(params["function"]))
 	groupName, _ := strconv.Unquote(string(params["group"]))
 
+	var response Response
+
 	switch function {
 	case "ADD":
-		add(groupName, member)
+		// convert object to Member struct
+		var member Member
+		err = json.Unmarshal(params["object"], &member)
+		if err != nil {
+			log.Println("Unable to parse object ", err)
+			return
+		}
+		response = add(groupName, member)
 	case "GET":
-		fmt.Printf("Trying to GET")
+		response = get(groupName)
 	case "DELETE":
-		fmt.Printf("Trying to DELETE")
+		memberName, _ := strconv.Unquote(string(params["object"]))
+		response = deleteMember(groupName, memberName)
 	case "DELETE_GROUP":
-		fmt.Printf("Trying to DELETE")
+		response = deleteGroup(groupName)
+	default:
+		response = Response{Status: "error", Message: "not a valid function"}
 	}
+
+	// Convert Response struct into json and return response
+	js, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
 
-func add(groupName string, member Member) {
-	fmt.Println("Called ADD with", groupName, member)
+func add(groupName string, member Member) Response {
 	groups := loadGroups()
 
 	_, found := find(groups, groupName)
 	if found {
 		// load group
 		group := loadGroup(groupName)
+		// add member to map
 		group.Members[member.Name] = member
-		// add member
-		// TODO save group
+		saveGroup(group)
 	} else {
 		groups = append(groups, groupName)
-		// new group struct
+		// new Member and Group struct
 		members := map[string]Member{member.Name: member}
 		group := Group{Name: groupName, Members: members}
-		// new file
-		fmt.Println("Group:", group)
-		// save group
-		// save list of groups
+
+		saveGroup(group)
+		saveGroups(groups)
 	}
 
-	fmt.Println("Groups:", groups)
+	response := Response{Status: "success", Message: "member was added to group"}
+	return response
+}
+
+func get(groupName string) Response {
+	groups := loadGroups()
+	_, found := find(groups, groupName)
+	if found {
+		// Read group file and convert to string
+		file, _ := ioutil.ReadFile(groupName + ".json")
+		groupStr := string(file)
+		response := Response{Status: "success", Message: groupStr}
+		return response
+	} else {
+		response := Response{Status: "error", Message: "group not found"}
+		return response
+	}
+}
+
+func deleteMember(groupName string, memberName string) Response {
+	groups := loadGroups()
+
+	_, found := find(groups, groupName)
+	if found {
+		// load group
+		group := loadGroup(groupName)
+		// Check if member exists, delete if so
+		if _, found = group.Members[memberName]; found {
+			delete(group.Members, memberName)
+			// delete entire group if no more members
+			if len(group.Members) <= 0 {
+				_ = deleteGroup(groupName)
+			} else {
+				saveGroup(group)
+			}
+			response := Response{Status: "success", Message: "member was deleted from group"}
+			return response
+
+		} else {
+			response := Response{Status: "error", Message: "member not found in the group"}
+			return response
+		}
+	} else {
+		response := Response{Status: "error", Message: "group not found"}
+		return response
+	}
+}
+
+func deleteGroup(groupName string) Response {
+	groups := loadGroups()
+
+	i, found := find(groups, groupName)
+	if found {
+		// replace group with last item and splice to be 1 shorter
+		groups[i] = groups[len(groups)-1]
+		groups[len(groups)-1] = ""
+		groups = groups[:len(groups)-1]
+		saveGroups(groups)
+		_ = os.Remove(groupName + ".json")
+		response := Response{Status: "success", Message: "group was deleted"}
+		return response
+	} else {
+		response := Response{Status: "error", Message: "group not found"}
+		return response
+	}
 }
 
 func loadGroups() []string {
+	file, _ := ioutil.ReadFile("groups.json")
 	var groups []string
+	_ = json.Unmarshal([]byte(file), &groups)
 	return groups
 }
 
 func loadGroup(groupName string) Group {
-	return Group{Name: groupName, Members: map[string]Member{}}
+	file, _ := ioutil.ReadFile(groupName + ".json")
+	var group Group
+	_ = json.Unmarshal([]byte(file), &group)
+	return group
+}
+
+func saveGroup(group Group) {
+	file, _ := json.MarshalIndent(group, "", " ")
+	_ = ioutil.WriteFile(group.Name+".json", file, 0644)
+}
+
+func saveGroups(groups []string) {
+	file, _ := json.MarshalIndent(groups, "", " ")
+	_ = ioutil.WriteFile("groups.json", file, 0644)
 }
 
 func find(slice []string, val string) (int, bool) {
